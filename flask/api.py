7185bbec2ca1f6149f9c855ebf8beeccb6cb0942
Tara Balakrishnan <tarabalakrishnan@DN525fbi.SUNet>
2016-04-16 18:00:32 -0700

    added local flask server code

---
+++ flask/api.py
@@ -0,0 +1,28 @@
+#  api.py
+#  flask
+#
+#  Created by Tara Balakrishnan on 4/16/16.
+#  Copyright 2016 Cargi. All rights reserved.
+#
+#  Local Flask Server initialized
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+todos = {}
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
