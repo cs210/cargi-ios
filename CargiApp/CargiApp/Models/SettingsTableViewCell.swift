74658b6d56afdd262f02a5dea69da952f1c76bd0
Ishita Prasad <ishitap@stanford.edu>
2016-04-17 22:16:56 -0700

    added Settings table view cells

---
+++ CargiApp/CargiApp/Models/SettingsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsTableViewCell.swift
+//  Cargi
+//
+//  Created by Ishita Prasad on 4/17/16.
+//  Copyright © 2016 Cargi. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
