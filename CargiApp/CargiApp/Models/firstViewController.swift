871a2aa03550914f0e2171b8d55d49fd1eab5788
Ishita Prasad <ishitap@stanford.edu>
2016-04-19 20:38:55 -0700

    initial setup of new main view

---
+++ CargiApp/CargiApp/Models/firstViewController.swift
@@ -0,0 +1,44 @@
+//
+//  firstViewController.swift
+//  Cargi
+//
+//  Created by Ishita Prasad on 4/19/16.
+//  Copyright © 2016 Cargi. All rights reserved.
+//
+
+import UIKit
+
+class firstViewController: UIViewController {
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillDisappear(animated: Bool){
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
