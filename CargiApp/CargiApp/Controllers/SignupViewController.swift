b69f7162b15eaf88d8b017209ab8db32e53fd749
Edwin Park <edpark@stanford.edu>
2016-04-30 23:41:28 -0700

    login view and signup view designed on storyboard

---
+++ CargiApp/CargiApp/Controllers/SignupViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SignupViewController.swift
+//  Cargi
+//
+//  Created by Edwin Park on 4/30/16.
+//  Copyright © 2016 Cargi. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.nameTextField.delegate = self
+        self.emailTextField.delegate = self
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if textField == emailTextField {
+            // email text field clicked "done"
+            textField.resignFirstResponder()
+            print("Go to the next page")
+            return true
+        } else if textField == nameTextField {
+            // name text field clicked "done"
+            textField.resignFirstResponder()
+            return true
+        }
+        return true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
