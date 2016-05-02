e9bcaeaef84847eff56ce6f03d66eb5eca70d5b7
Ishita Prasad <ishitap@stanford.edu>
2016-05-02 01:36:40 -0700

    set up scene for event picker

---
+++ CargiApp/CargiApp/EventPickerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  EventPickerViewController.swift
+//  Cargi
+//
+//  Created by Ishita Prasad on 5/2/16.
+//  Copyright © 2016 Cargi. All rights reserved.
+//
+
+import UIKit
+
+class EventPickerViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
