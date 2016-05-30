8f87c0f6fa42fe7913a6d47175dca6c35cbda271
Maya Balakrishnan <mayanb@gmail.com>
2016-05-30 00:45:47 -0700

    bluetooth test stuff

--- CargiApp/CargiApp/Controllers/NavigationViewController.swift
+++ CargiApp/CargiApp/Controllers/NavigationViewController.swift
@@ -73,6 +73,8 @@ class NavigationViewController: UIViewController, SKTransactionDelegate, CLLocat
     var waypoint: Location?
     
     var manager: CBCentralManager! // Bluetooth Manager
+    var peripheralArray = [CBPeripheral]()
+    var connectingPeripheral: CBPeripheral?
     var currentEvent: EKEvent? {
         didSet {
             currentEventButton.setTitle(currentEvent?.title, forState: .Normal)
@@ -125,13 +127,19 @@ class NavigationViewController: UIViewController, SKTransactionDelegate, CLLocat
     
     let stopWords: [String] = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
 
-    
+    // Put CentralManager in the main queue bluetooth
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        manager = CBCentralManager(delegate: self, queue: dispatch_get_main_queue())
+        
+    }
     
     // MARK: Methods
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        //        manager = CBCentralManager (delegate: self, queue: nil)
 
         // Do any additional setup after loading the view.
         UIApplication.sharedApplication().statusBarStyle = UIStatusBarStyle.LightContent
@@ -837,6 +845,12 @@ class NavigationViewController: UIViewController, SKTransactionDelegate, CLLocat
     // MARK: Core Bluetooth Manager Methods
     func centralManager(central: CBCentralManager, didDiscoverPeripheral peripheral: CBPeripheral, advertisementData: [String : AnyObject], RSSI: NSNumber) {
         print("Peripheral: \(peripheral)")
+        self.connectingPeripheral=peripheral
+        if (!self.peripheralArray.contains(peripheral)) {
+            self.peripheralArray.append(peripheral)
+        }
+        print(peripheralArray)
+        
     }
     
     func centralManager(central: CBCentralManager, didConnectPeripheral peripheral: CBPeripheral) {
@@ -864,7 +878,6 @@ class NavigationViewController: UIViewController, SKTransactionDelegate, CLLocat
             manager.scanForPeripheralsWithServices(nil, options: nil)
         }
     }
-    
     // MARK: GMSMapViewDelegate Methods
     
     
