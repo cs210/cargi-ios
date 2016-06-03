7a878aea888ddc5d3ebe48fc203da57370a71850
Ishita Prasad <ishitap@stanford.edu>
2016-06-02 19:05:34 -0700

    added error checking to makes sure you cant swipe too much

--- CargiApp/CargiApp/Controllers/NavigationViewController.swift
+++ CargiApp/CargiApp/Controllers/NavigationViewController.swift
@@ -136,7 +136,11 @@ class NavigationViewController: UIViewController, SKTransactionDelegate, CLLocat
         }
     }
     
+    var currentDashOption = 2
+    
     // MARK: Constants
+    
+    let numDashOptions = 4
     
     let stopWords: [String] = ["a", "about", "above", "after", "again", "against", "all", "am", "an", "and", "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself", "yourselves"]
 
@@ -212,8 +216,13 @@ class NavigationViewController: UIViewController, SKTransactionDelegate, CLLocat
     }
     
     func swipeLeft(recognizer : UISwipeGestureRecognizer) {
+    
+        if (currentDashOption == (numDashOptions - 1)) {
+            return
+        }
         
         UIView.animateWithDuration(0.5, delay: 0.0, options: .CurveEaseOut, animations: {
+            
             var dashboardViewFrame = self.dashboardView.frame
             let screenSize: CGRect = UIScreen.mainScreen().bounds
             let screenWidth = screenSize.width
@@ -225,11 +234,15 @@ class NavigationViewController: UIViewController, SKTransactionDelegate, CLLocat
             self.dashboardView.frame = dashboardViewFrame
             
             }, completion: { finished in
-                print("swiped")
+                self.currentDashOption++
         })
     }
     
     func swipeRight(recognizer : UISwipeGestureRecognizer) {
+        
+        if (currentDashOption == 2) {
+            return
+        }
         
         UIView.animateWithDuration(0.5, delay: 0.0, options: .CurveEaseOut, animations: {
             var dashboardViewFrame = self.dashboardView.frame
@@ -243,7 +256,7 @@ class NavigationViewController: UIViewController, SKTransactionDelegate, CLLocat
             self.dashboardView.frame = dashboardViewFrame
             
             }, completion: { finished in
-                print("swiped")
+                self.currentDashOption--
         })
     }
     
